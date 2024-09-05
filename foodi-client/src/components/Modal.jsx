e554feb882ae08e63b0dd24e08bba7d9af5d95ac
vinshavinod <vinshavinod3@gmail.com>
2024-09-05 15:02:44 +0530

    Food_App

---
+++ foodi-client/src/components/Modal.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { AuthContext } from '../contexts/AuthProvider';
+
+const Modal = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { signUpWithGmail,login} = useContext(AuthContext);
+    const [errorMessage, setErrorMessage] = useState("");
+  
+    //redirecting to home page
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || "/";
+    
+    const onSubmit = data => {
+      const email = data.email;
+      const password = data.password;
+      //console.log(email, password);
+      login(email, password).then((result) => {
+        const user = result.user;
+        alert("login successfull");
+
+        document.getElementById('my_modal_5').close()
+        navigate(from, {replace: true})
+      }).catch((error) => {
+        const errorMessage = error.message;
+        setErrorMessage("provide a correct email and password")
+      })
+    };
+
+    // Google signIn
+    const handleLogin = () => {
+        signUpWithGmail().then((result) => {
+            const user = result.user;
+            alert("Login successful");
+            navigate(from, {replace: true})
+        }).catch((error) => console.log(error));
+    };
+
+    return (
+        <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
+            <div className="modal-box">
+                <div className="modal-action flex flex-col justify-center mt-0">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body" method="dialog">
+                        <h3 className='font-bold text-lg'>Please Login</h3>
+
+                        {/* Email */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
+                            {errors.email && <p className="text-red-500">Email is required</p>}
+                        </div>
+
+                        {/* Password */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
+                            {errors.password && <p className="text-red-500">Password is required</p>}
+                            <label className="label mt-1">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+
+                        {/* error */}
+                          {
+                            errorMessage ?  <p className="text-red text-xs italic">{errorMessage}</p> : ""
+
+                          }
+                        {/* Login Button */}
+                        <div className="form-control mt-4">
+                            <input type="submit" value="Login" className="btn bg-green text-white" />
+                        </div>
+
+                        <p className='text-center my-2'>Do not have an account? <Link to='/signup' className='underline text-right ml-1'>Signup Now</Link></p>
+
+                        <button
+                            htmlFor="my_modal_5"
+                            onClick={() => document.getElementById('my_modal_5').close()}
+                            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <div className='text-center space-x-3 mb-5'>
+                        <button onClick={handleLogin} className="btn btn-circle hover:bg-green hover:text-white">
+                            <FaGoogle />
+                        </button>
+                        <button className="btn btn-circle hover:bg-green hover:text-white">
+                            <FaFacebookF />
+                        </button>
+                        <button className="btn btn-circle hover:bg-green hover:text-white">
+                            <FaGithub />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </dialog>
+    );
+};
+
+export default Modal;
